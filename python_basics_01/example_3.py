285cac625bae6e4ed729d8811e7cdb7475bc8a12
Aakash Verma <dev.aakashv@gmail.com>
2022-01-01 23:09:56 +0530

    add the `example_3` of the lesson

---
+++ python_basics_01/example_3.py
@@ -0,0 +1,9 @@
+x = 10
+y = 5
+
+addition = x + y       # 10 + 5 = 15
+subtraction = x - y    # 10 - 5 = 5
+multiplication = x * y # 10 * 5 = 50
+division = x / y       # 10 / 5 = 2.0 (always returns a float)
+
+print(addition, subtraction, multiplication, division)
