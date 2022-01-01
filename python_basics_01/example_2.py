08617e071df116a4bd950dea132a8eeecd259fd7
Aakash Verma <dev.aakashv@gmail.com>
2022-01-01 23:08:46 +0530

    add the `example_2` of the lesson

---
+++ python_basics_01/example_2.py
@@ -0,0 +1,9 @@
+my_integer = 42
+my_float = 3.14
+my_string = "Hello, Python!"
+my_boolean = True
+
+print(type(my_integer))  # Output: <class 'int'>
+print(type(my_float))    # Output: <class 'float'>
+print(type(my_string))   # Output: <class 'str'>
+print(type(my_boolean))  # Output: <class 'bool'>
