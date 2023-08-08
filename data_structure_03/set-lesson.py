58551cf550f76d0b883f77b7dcddb4dadaff9083
Aakash Verma <dev.aakashv@gmail.com>
2023-08-08 13:49:13 +0530

    `feat`: alter the `set` behavior

--- data_structure_03/set-lesson.py
+++ data_structure_03/set-lesson.py
@@ -20,3 +20,51 @@ print(number_set)
 number_set = { 1, 2, 3, 4, 4, 5, 5}
 print(number_set)
 # Output: { 1, 2, 3, 4, 5 }
+
+#==============================================
+
+my_set = {1}
+
+# Add the value to set
+
+my_set.add('Aakash')
+my_set.add(2)
+my_set.add(3)
+my_set.add('Verma')
+print(my_set)
+# Output: {1, 2, 3, 'Aakash', 'Verma'}
+
+#==============================================
+
+my_set.remove('Aakash')
+my_set.remove(2)
+my_set.remove(3)
+my_set.remove('Verma')
+my_set.remove(1)
+print(my_set)
+# Output: set()
+
+#=============================================
+
+# Edge case
+
+'''
+If you want to create an empty set and later add the values
+then below is not possible
+
+variable = {}
+
+variable.add('Aakash')
+
+because behind the scene after definition `variable = {}`,
+python consider variable of type dictionary not `Set`. So in
+order to solve this problem. We need to explicitely tell
+the interpreter we need an empty Set for later use.
+
+'''
+
+variable = set()
+
+variable.add('Aakash')
+print(variable)
+# Output: { 'Aakash' }
